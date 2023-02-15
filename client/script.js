e2b50821afc0d199dca4b5ee3ada687c9c276926
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-02-15 14:45:30 +0700

    update the server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://open-ai-chat-epa6.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
