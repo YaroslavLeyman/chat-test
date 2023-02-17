10586410993e29ba5250243d3001bde772bcb01c
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-02-17 15:47:31 +0700

    Test

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // извлечение данных с сервера -> ответ бота
-  const response = await fetch('https://open-ai-chat-epa6.onrender.com', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
